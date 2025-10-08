fe2c8003c299c49fc237299300b292cabb17fc11
Somadina <kingswatter@gmail.com>
2025-10-08 15:26:07 +0100

    layout, pages, still trying to fix the dark mode. Will defer it until some other time, as it is affecting my progress

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,36 +1,40 @@
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
-import AppLayout from '@/layouts/app-layout';
-import { dashboard } from '@/routes';
-import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/react';
-
-const breadcrumbs: BreadcrumbItem[] = [
-    {
-        title: 'Dashboard',
-        href: dashboard().url,
-    },
-];
+import DemographicCard from "@/components/ecommerce/DemographicCard";
+import EcommerceMetrics from "@/components/ecommerce/EcommerceMetrics";
+import MonthlySalesChart from "@/components/ecommerce/MonthlySalesChart";
+import MonthlyTarget from "@/components/ecommerce/MonthlyTarget";
+import RecentOrders from "@/components/ecommerce/RecentOrders";
+import StatisticsChart from "@/components/ecommerce/StatisticsChart";
+import AppLayout from "@/layouts/AppLayout";
+import React from 'react';
 
 export default function Dashboard() {
     return (
-        <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Dashboard" />
-            <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
+        <>
+            <div className="grid grid-cols-12 gap-4 md:gap-6">
+                <div className="col-span-12 space-y-6 xl:col-span-7">
+                    <EcommerceMetrics />
+
+                    <MonthlySalesChart />
                 </div>
-                <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+
+                <div className="col-span-12 xl:col-span-5">
+                    <MonthlyTarget />
+                </div>
+
+                <div className="col-span-12">
+                    <StatisticsChart />
+                </div>
+
+                <div className="col-span-12 xl:col-span-5">
+                    <DemographicCard />
+                </div>
+
+                <div className="col-span-12 xl:col-span-7">
+                    <RecentOrders />
                 </div>
             </div>
-        </AppLayout>
+        </>
     );
 }
+
+Dashboard.layout = (page: React.ReactNode) => <AppLayout>{page}</AppLayout>;
--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,15 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::get('/form-elements', function () {
+    return Inertia::render('Forms/FormElements');
+})->name('home');
+
+Route::middleware([])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
